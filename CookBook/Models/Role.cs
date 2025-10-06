6a15169cdd29b5b53b3415f75bbb77835a30a663
CdelO19 <delaunois.celia@gmail.com>
2025-10-06 09:43:47 +0200

    Création de la classe rôle

---
+++ CookBook/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace CookBook.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string nom_role { get; set; }
+        /* Les Rôles : Admin et Utilisateur */
+    }
+}
