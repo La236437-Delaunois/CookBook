536b4c6f18d8e9334d2c020a0aa137db55d00a1b
CdelO19 <delaunois.celia@gmail.com>
2025-10-13 16:31:44 +0200

    ajout d'une liste pour les utilisateurs

--- CookBook/Models/Role.cs
+++ CookBook/Models/Role.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string nom_role { get; set; }
         /* Les Rôles : Admin et Utilisateur */
+        public ICollection<Utilisateur> Utilisateurs { get; set; }
     }
 }
