a94458fd5a849f1ebc67a867e5d3b7ddac87c416
CdelO19 <delaunois.celia@gmail.com>
2025-10-13 16:35:20 +0200

    ajout de l'attribut RoleId

--- CookBook/Models/Utilisateur.cs
+++ CookBook/Models/Utilisateur.cs
@@ -6,6 +6,8 @@
         public string Pseudo { get; set; }
         public string Email { get; set; }
         public string MotDePasse { get; set; }
+
+        public int RoleId { get; set; }
         public Role Role { get; set; }
     }
 }
