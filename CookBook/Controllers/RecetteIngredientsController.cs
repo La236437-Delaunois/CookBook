65bce036ac68573af85ae6288e2137a1aa34aa66
la237834 <la237834@student.helha.be>
2025-10-28 15:28:38 +0100

    modification des controllers liés aux recettes pour les rendre inaccessible déconnecté

--- CookBook/Controllers/RecetteIngredientsController.cs
+++ CookBook/Controllers/RecetteIngredientsController.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CookBook.Data;
+using CookBook.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CookBook.Data;
-using CookBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CookBook.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class RecetteIngredientsController : ControllerBase
