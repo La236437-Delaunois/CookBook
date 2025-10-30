8e06ebebc40f104cd4098c4e836d8cf7ec9ddc1b
la237834 <la237834@student.helha.be>
2025-10-30 11:17:05 +0100

    ajout d'une méthode pour rechercher les recettes par catégorie (en vue d'ajouter des filtres)

--- CookBook/Controllers/RecettesController.cs
+++ CookBook/Controllers/RecettesController.cs
@@ -44,6 +44,16 @@ namespace CookBook.Controllers
             return recette;
         }
 
+        // GET: api/Recettes/Categorie/5
+        [HttpGet("Categorie/{categorieId}")]
+        public async Task<ActionResult<IEnumerable<Recette>>> GetRecettesByCategorie(int categorieId)
+        {
+            var recettes = await _context.Recette
+                                         .Where(r => r.CategorieId == categorieId)
+                                         .ToListAsync();
+            return recettes;
+        }
+
         // PUT: api/Recettes/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
