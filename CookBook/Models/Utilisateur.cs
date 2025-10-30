65ebc32b1d017a69a83101edc0cdae99095b453c
CdelO19 <delaunois.celia@gmail.com>
2025-10-30 10:55:19 +0100

    Modification du ValidateNever en JsonIgnore

--- CookBook/Models/Utilisateur.cs
+++ CookBook/Models/Utilisateur.cs
@@ -11,7 +11,7 @@ namespace CookBook.Models
         public string MotDePasse { get; set; }
 
         public int RoleId { get; set; }
-        [ValidateNever]
+        [JsonIgnore]
         public Role Role { get; set; }
 
         /* Listes de recette*/
