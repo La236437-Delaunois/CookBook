4f2a4152cf7f3ff6831878d20d643e925e18d77f
CdelO19 <delaunois.celia@gmail.com>
2025-11-10 10:20:19 +0100

    Création du component PageArrive

---
+++ Bookify/src/app/page-arrive/page-arrive.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-arrive',
+  imports: [],
+  templateUrl: './page-arrive.html',
+  styleUrl: './page-arrive.css',
+})
+export class PageArrive {
+
+}
