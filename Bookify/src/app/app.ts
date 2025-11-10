5e7fa1c0aeb9e3d632ab1e4b65e33d3e5bd6d338
LA237484-Magonet <la237484@student.helha.be>
2025-11-10 16:30:54 +0100

    navbar de cree

--- Bookify/src/app/app.ts
+++ Bookify/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PageArrive } from "./page-arrive/page-arrive";
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PageArrive],
+  imports: [RouterOutlet, PageArrive,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
