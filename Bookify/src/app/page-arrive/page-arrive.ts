6bcdb8aa73d9e37a0f2c48b93ea9efb4e977db06
CdelO19 <delaunois.celia@gmail.com>
2025-11-10 13:46:26 +0100

    Installation du package Material et insertion du material bouton

--- Bookify/src/app/page-arrive/page-arrive.ts
+++ Bookify/src/app/page-arrive/page-arrive.ts
@@ -1,8 +1,9 @@
+import {MatButtonModule} from '@angular/material/button';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-arrive',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './page-arrive.html',
   styleUrl: './page-arrive.css',
 })
