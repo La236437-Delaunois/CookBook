2f0d01f881bfd9889b97026064be09c1eb1a3ba4
CdelO19 <delaunois.celia@gmail.com>
2025-11-10 15:10:50 +0100

    Déplacement des images et ajout de code pour la page d'arrivée

--- Bookify/src/app/app.ts
+++ Bookify/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageArrive } from "./page-arrive/page-arrive";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageArrive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
