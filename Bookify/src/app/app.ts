d874192a1a7f1ba8301e3081c825840f242980c7
Lea vdb <la237834@student.helha.be>
2025-11-10 17:32:44 +0100

    ajout du front pour la page de login sans connexion avec le backend

--- Bookify/src/app/app.ts
+++ Bookify/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PageArrive } from "./page-arrive/page-arrive";
+import { Login } from './auth/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PageArrive],
+  imports: [RouterOutlet, PageArrive,Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Bookify/src/app/auth/login/login.ts
+++ Bookify/src/app/auth/login/login.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 }
