0b62a9a0db8bf53e2a9edd31bb92494028f68754
Lea vdb <la237834@student.helha.be>
2025-11-17 16:40:49 +0100

    ajout des méthodes dans les services + de l'interface gender

--- Bookify/src/app/services/book.ts
+++ Bookify/src/app/services/book.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Review } from './review';
+import { Gender } from './gender';
+
+export interface Book {
+  id: number; 
+  title: string;     
+  author: string;      
+  isbn: string;       
+  price: number;      
+  description: string;  
+  publisher: string;    
+  genderId: number;     
+  gender?: Gender;     
+  reviews?: Review[];  
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private apiUrl = 'https://localhost:7079/api/';
+  private apiUrl = 'https://localhost:7079/api/book';
+
+  constructor(private http: HttpClient) {}
+
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  getBookById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  }
+
 }
