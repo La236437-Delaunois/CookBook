52f20bec7c1c50c92cb598d9ca09ea1a85291a94
LA237484-Magonet <la237484@student.helha.be>
2025-11-17 19:26:27 +0100

    ajout de la route et aussi de la laision entre les deuc

--- Bookify/src/app/acceuil/acceuil.ts
+++ Bookify/src/app/acceuil/acceuil.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CarteLivre } from '../carte-livre/carte-livre';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-acceuil',
-  imports: [],
+  standalone: true,
+  imports: [NgFor,CarteLivre],
   templateUrl: './acceuil.html',
   styleUrl: './acceuil.css',
 })
 export class Acceuil {
+ livres = [
+  {
+    title: "La belle au bois dormant",
+    genres: ["Fantasy", "Roman"]
+  },
+  {
+    title: "Fahrenheit 451",
+    genres: ["Sci-fi", "Roman"]
+  },
+  {
+    title: "La femme de ménage",
+    genres: ["Thriller", "Policier"]
+  }
+];
 
 }
