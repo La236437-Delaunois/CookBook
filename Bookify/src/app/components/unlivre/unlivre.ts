f66ba501e44c4cab87a4b9afbf3ea0e1c78a9cd5
CdelO19 <delaunois.celia@gmail.com>
2025-11-17 11:00:26 +0100

    Création du component livre pour réaliser la liste

---
+++ Bookify/src/app/components/unlivre/unlivre.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unlivre',
+  imports: [],
+  templateUrl: './unlivre.html',
+  styleUrl: './unlivre.css',
+})
+export class Unlivre {
+
+}
