86e2fdeb0bb1ed27332e21733a0b492798b1e131
CdelO19 <delaunois.celia@gmail.com>
2025-11-17 21:23:09 +0100

    Ajout du service vers l'api gender

--- Bookify/src/app/services/gender.ts
+++ Bookify/src/app/services/gender.ts
@@ -1,7 +1,21 @@
-import { Book } from "./book";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
 
 export interface Gender {
   id: number;  
-  name: string; 
-  books?: Book[];   
+  name: string;   
+}
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GenderService {
+  private apiUrl = 'https://localhost:7079/api/Genders';
+  constructor(private http: HttpClient) {}
+
+  getAllGenders(): any {
+    return this.http.get<any>(this.apiUrl);
+  } 
 }
