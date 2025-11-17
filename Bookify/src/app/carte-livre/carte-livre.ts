04d92a20993e88c076d7d017f88fdd8482cf499f
LA237484-Magonet <la237484@student.helha.be>
2025-11-17 18:53:27 +0100

    ajout du componnent pour la  carte d'un livre

---
+++ Bookify/src/app/carte-livre/carte-livre.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carte-livre',
+  imports: [],
+  templateUrl: './carte-livre.html',
+  styleUrl: './carte-livre.css',
+})
+export class CarteLivre {
+
+}
