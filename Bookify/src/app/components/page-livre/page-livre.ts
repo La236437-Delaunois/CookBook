fe2683eb6e261d28ccc92fa5fec1764dadeaf372
CdelO19 <delaunois.celia@gmail.com>
2025-11-17 10:09:48 +0100

    Ajout du titre et du css de la page gestion de livres

--- Bookify/src/app/components/page-livre/page-livre.ts
+++ Bookify/src/app/components/page-livre/page-livre.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../../navbar/navbar";
 
 @Component({
   selector: 'app-page-livre',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './page-livre.html',
   styleUrl: './page-livre.css',
 })
