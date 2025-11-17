7bde6c8f24d30dd5beb137ac6fa2ad9d1c4f13fd
LA237484-Magonet <la237484@student.helha.be>
2025-11-17 14:23:01 +0100

    ajout de la verfication etre les deux mdp

--- Bookify/src/app/inscription/inscription.ts
+++ Bookify/src/app/inscription/inscription.ts
@@ -1,13 +1,33 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Login } from '../login/login';
+
 
 @Component({
   selector: 'app-inscription',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink,FormsModule],
   templateUrl: './inscription.html',
   styleUrl: './inscription.css',
 })
 export class Inscription {
+  name = '';
+  username = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  errorMessage = '';
 
+    onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Les mots de passe ne correspondent pas.';
+      return;
+    }
+
+    this.errorMessage = '';
+    console.log('Formulaire OK :');
+
+
+  }
 }
+ 
