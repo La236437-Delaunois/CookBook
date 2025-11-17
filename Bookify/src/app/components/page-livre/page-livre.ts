d7e20c3c7318348e9a5de5187755aeefa423d4ed
CdelO19 <delaunois.celia@gmail.com>
2025-11-17 11:07:17 +0100

    Ajout du component livre dans la page livre

--- Bookify/src/app/components/page-livre/page-livre.ts
+++ Bookify/src/app/components/page-livre/page-livre.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from "../../navbar/navbar";
 import { BoutonRecherche } from "../bouton-recherche/bouton-recherche";
 import { AjouterLivre } from "../ajouter-livre/ajouter-livre";
+import { Unlivre } from '../unlivre/unlivre';
 
 @Component({
   selector: 'app-page-livre',
-  imports: [Navbar, BoutonRecherche, AjouterLivre],
+  imports: [Navbar, BoutonRecherche, AjouterLivre, Unlivre],
   templateUrl: './page-livre.html',
   styleUrl: './page-livre.css',
 })
