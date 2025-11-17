74fc8ff19165d8783d6917c3430cc1785cd759c6
CdelO19 <delaunois.celia@gmail.com>
2025-11-17 09:54:05 +0100

    Création du component de la page de gestion de livres de l'admin

---
+++ Bookify/src/app/components/page-livre/page-livre.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-livre',
+  imports: [],
+  templateUrl: './page-livre.html',
+  styleUrl: './page-livre.css',
+})
+export class PageLivre {
+
+}
