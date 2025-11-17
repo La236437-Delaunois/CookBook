3a358c39373b28e3f05640a7395f789a2278114f
Lea vdb <la237834@student.helha.be>
2025-11-17 16:00:00 +0100

    ajout des liens vers l'api + fix des noms

--- Bookify/src/app/services/review.ts
+++ Bookify/src/app/services/review.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserService } from './user';
+import { BookService } from './book';
+
+export interface Review{
+  id: number;
+  bookId: number;
+  book?: BookService;
+  userId: number;
+  user?: UserService;
+  note: number;
+  titre?: string;
+  avis: string;  
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class Review {
-  
+export class ReviewService {
+  apiUrl = "http://localhost:7079/api/reviews";
+
+  constructor(private http: HttpClient) {}
+
+  addReview(review: Review): Observable<Review> {
+    return this.http.post<Review>(this.apiUrl, review);
+  }
+
+  updateReview(id: number, review: Review): Observable<Review> {
+    return this.http.put<Review>(`${this.apiUrl}/${id}`, review);
+  }
+
+  deleteReview(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
--- Bookify/src/app/services/user.spec.ts
+++ Bookify/src/app/services/user.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { User } from '../services/user';
+import { User, UserService } from '../services/user';
 
 describe('User', () => {
-  let service: User;
+  let service: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(User);
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
