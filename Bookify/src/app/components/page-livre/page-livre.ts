42f3c5f7e9bb2deb8cf1b729c08ed301a854767e
CdelO19 <delaunois.celia@gmail.com>
2025-11-17 10:13:41 +0100

    Création du bouton de la recherche et ajout dans la page

--- Bookify/src/app/components/page-livre/page-livre.ts
+++ Bookify/src/app/components/page-livre/page-livre.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../navbar/navbar";
+import { BoutonRecherche } from "../bouton-recherche/bouton-recherche";
 
 @Component({
   selector: 'app-page-livre',
-  imports: [Navbar],
+  imports: [Navbar, BoutonRecherche],
   templateUrl: './page-livre.html',
   styleUrl: './page-livre.css',
 })
