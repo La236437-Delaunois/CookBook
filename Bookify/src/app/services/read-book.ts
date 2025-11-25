230d0e20f80df5ab3db7e508bdda7ca5da477ef0
la237834 <la237834@student.helha.be>
2025-11-25 20:55:10 +0100

    ajout du cookie en constante au lieu de passer en paramètre l'id

--- Bookify/src/app/services/read-book.ts
+++ Bookify/src/app/services/read-book.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 export interface ReadBook {
   id: number;
@@ -20,17 +21,20 @@ export interface ReadBook {
 export class ReadBookService {
   private apiUrl = 'http://localhost:5211/api/LivreLu';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
-  getReadBooks(userId: number): Observable<ReadBook[]> {
+  getReadBooks(): Observable<ReadBook[]> {
+    const userId = this.cookieService.get('userId');
     return this.http.get<ReadBook[]>(`${this.apiUrl}/${userId}`);
   }
 
-  addReadBook(bookId: number, userId: number): Observable<any> {
+  addReadBook(bookId: number): Observable<any> {
+    const userId = this.cookieService.get('userId');
     return this.http.post(`${this.apiUrl}/${bookId}/${userId}`, {});
   }
 
-  removeReadBook(bookId: number, userId: number): Observable<any> {
+  removeReadBook(bookId: number): Observable<any> {
+    const userId = this.cookieService.get('userId');
     return this.http.delete(`${this.apiUrl}/${bookId}/${userId}`);
   }
 
